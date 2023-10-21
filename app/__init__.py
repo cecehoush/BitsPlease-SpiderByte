3e0db786337748d0d74299f675dea00ffd1e1651
cecehoush <choush@msudenver.edu>
2023-10-21 16:11:39 -0600

    updateee

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import os
+
+app = Flask("Authentication Web App")
+# app.secret_key = os.environ['SECRET_KEY']
+app.secret_key = 'ooga'
+
+# initialize database
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db.init_app(app)
+
+from app import models
+with app.app_context(): 
+    db.create_all()
+
+# login manager
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app.models import User
+
+# user_loader callback
+@login_manager.user_loader
+def load_user(id):
+    try: 
+        return db.session.query(User).filter(User.id==id).one()
+    except: 
+        return None
+
+from app import routes
